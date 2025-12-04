dbae5dfd4608bb2a104bbcb8205cc1c64bdb0b1e
Vladyslav Barbar <barbar.vladyslav@lll.kpi.ua>
2025-12-04 22:53:44 +0200

    feat: Add TextureHandler and update dependencies

---
+++ TextureHandler.js
@@ -0,0 +1,27 @@
+function LoadTexture(url) {
+    var texture = gl.createTexture();
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT); // change to REPEAT for better tiling
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
+
+    var image = new Image();
+    image.crossOrigin = 'anonymous';
+    image.src = url;
+    image.addEventListener('load', () => {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        
+        // Generate mipmaps for better quality
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+        
+        if (typeof draw === 'function') draw();
+    });
+
+    return texture;
+}
